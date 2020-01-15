2b1eac21dd1e868eb36aa08c2bc4caaf3d5de736
limsoyoung <soyim359@gmail.com>
2020-01-15 17:30:34 +0900

    지뢰찾기 깃발 꽂기,  주변 지뢰개수 표시하기 완료

--- html/11_landmine.js
+++ html/11_landmine.js
@@ -1,4 +1,8 @@
+let tbody = document.querySelector('table tbody');
+let dataset = [];
+
 document.querySelector('#exc').addEventListener('click',function(){
+    tbody.innerHTML = '';
     let hor = document.querySelector('#hor').value;
     let ver = document.querySelector('#ver').value;
     let mine = document.querySelector('#mine').value;
@@ -11,27 +15,70 @@ document.querySelector('#exc').addEventListener('click',function(){
 
     //셔플에 값을 하나씩 뽑아서 넣음 (지뢰 위치 뽑기)
     let suffle = [];
-    while(list.length > 80){
+    while(list.length > (hor * ver - mine)){
         let move = list.splice(Math.floor(Math.random() * list.length), 1)[0];
         suffle.push(move);
     }
     console.log(suffle);
 
-    let dataset = [];
-    let tbody = document.querySelector('table tbody');
+
     for(let i = 0; i < ver; i++){
         let arr = [];
         let tr = document.createElement('tr');
         dataset.push(arr);
         for(let j = 0; j < hor; j++){
             let td = document.createElement('td');
+            td.addEventListener('contextmenu', function(e){
+                e.preventDefault();
+                // console.log('우클릭');
+                let 부모tr = e.currentTarget.parentNode;
+                let 부모tbody = e.currentTarget.parentNode.parentNode;
+                let 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);  
+                let 줄 = Array.prototype.indexOf.call(부모tbody.children, e.currentTarget.parentNode);  
+                console.log(줄, 칸);
+                if(e.currentTarget.textContent === '' || e.currentTarget.textContent === 'x'){
+                    e.currentTarget.textContent = '!';
+                }else if(e.currentTarget.textContent === '!'){
+                    e.currentTarget.textContent = '?';
+                }else if(e.currentTarget.textContent === "?"){
+                    if(dataset[줄][칸] === 1){
+                        e.currentTarget.textContent = '';
+                    }else if(dataset[줄][칸] === 'x'){
+                        e.currentTarget.textContent = 'x';
+                    }
+                }
+                // console.log(dataset);
+            });
+            td.addEventListener('click', function(e){
+                //클릭했을때 주변 지뢰 개수
+                let 부모tr = e.currentTarget.parentNode;
+                let 부모tbody = e.currentTarget.parentNode.parentNode;
+                let 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);  
+                let 줄 = Array.prototype.indexOf.call(부모tbody.children, e.currentTarget.parentNode);  
+                if(dataset[줄][칸] === 'x'){
+                    e.currentTarget.textContent = '펑!';
+                }else{
+                    let 주변 = [                       
+                        dataset[줄][칸-1],                    , dataset[줄][칸+1],
+                    ];
+                    if(dataset[줄-1]){
+                        주변 = 주변.concat([dataset[줄-1][칸-1], dataset[줄-1][칸], dataset[줄-1][칸+1]]);
+                    }else if(dataset[줄+1]){
+                        주변 = 주변.concat([dataset[줄+1][칸-1], dataset[줄+1][칸], dataset[줄+1][칸+1]]);
+                    }
+                    console.log(주변);
+                    e.currentTarget.textContent = 주변.filter(function(v){
+                        return v === 'x';
+                    }).length;
+                }
+            });
             arr.push(1);
             tr.appendChild(td);
             // td.textContent = 1;
         }
         tbody.appendChild(tr);
     }
-    console.log(dataset);
+    // console.log(dataset);
 
     //지뢰 심기
     for (let k=0; k < suffle.length; k++){       // 예 60
@@ -43,3 +90,4 @@ document.querySelector('#exc').addEventListener('click',function(){
     }
     console.log(dataset);
 });
+
