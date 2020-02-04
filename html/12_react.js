43775c45380ca6349d95a74ddd73397c08d7958f
limsoyoung <soyim359@gmail.com>
2020-02-04 17:16:40 +0900

    반응속도 시간 재기

--- html/12_react.js
+++ html/12_react.js
@@ -4,17 +4,27 @@ var status = {
 };
 
 
+
 screen1.addEventListener('click',function(){
+    var startTime;
+
     if(screen1.classList.contains('waiting')){  //현재 준비상태인지 파악
         // console.log('클릭');
         screen1.classList.remove('waiting');
         screen1.classList.add('ready');
         screen1.textContent = '초록색이 되면 클릭하세요';
+        setTimeout(function(){
+            //시간 재기
+            startTime = new Date(); 
+            screen1.click();
+        }, Math.floor(Math.random() * 1000) + 2000); //2000~ 3000사이의 수
     }else if(screen1.classList.contains('ready')){
         screen1.classList.remove('ready');
         screen1.classList.add('now');
         screen1.textContent = '클릭하세요!';
     }else if(screen1.classList.contains('now')){
+        var endTime = new Date();
+        console.log('반응속도',(endTime - startTime)/1000, 's');
         screen1.classList.remove('now');
         screen1.classList.add('waiting');
         screen1.textContent = '클릭해서 시작하세요';
